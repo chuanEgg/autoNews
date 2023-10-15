15d29322f4477a5091d9e0d497a2e42ef34563ca
chuanEgg <honganchen53@gmail.com>
2023-10-15 21:52:43 +0800

    add .gitignore and other testing python files

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import torch
 
-generator = pipeline("text-generation", model="yentinglin/Taiwan-LLaMa-v1.0")
-output = generator(
-        "誰是傅斯年?",
-        max_length=30,
-        num_return_sequences=2,
-)
+from accelerate import infer_auto_device_map, init_empty_weights
+from transformers import AutoConfig, AutoModelForCausalLM
 
-print(output)
+model = "yentinglin/Taiwan-LLaMa-v1.0"
+
+quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
+tokenizer = AutoTokenizer.from_pretrained(model)
+model_8bit = AutoModelForCausalLM.from_pretrained(model, device_map="auto", load_in_8bit=True, quantization_config=quantization_config, offload_folder="offload", torch_dtype=torch.float16, offload_state_dict = True)
+
+# Get memory usage
+model_8bit.get_memory_footprint()
+
+max_tokens = 100
+input_ids = tokenizer("NTU 在哪?", return_tensors="pt").input_ids.to('cuda')
+outputs = model_8bit.generate(input_ids, max_new_tokens=max_tokens)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
