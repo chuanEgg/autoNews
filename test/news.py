6ad9cda742d52f7d4204c7cd628b1cabab74e26c
chuanEgg <honganchen53@gmail.com>
2023-10-18 22:03:55 +0800

    reformat

--- test/news.py
+++ test/news.py
@@ -1,7 +1,7 @@
 from gnews import GNews
 
 google_news = GNews(language="zh", country="TW", max_results=5)
-res = google_news.get_news('以色列')
+res = google_news.get_news('總統大選')
 for i in range(len(res)):
     try:
         article = google_news.get_full_article(res[i]["url"])
