708b10c77f328d798cf5cba05d1d0ce037c75436
muen1019 <muen1019@gmail.com>
2023-10-30 23:16:07 +0800

    Text to speech and video generating test

---
+++ text_to_speach_and_video_editing_test/video.py
@@ -0,0 +1,6 @@
+from moviepy.editor import *
+
+# clip = ImageClip("老鼠會.png").set_start(0).set_duration(5)
+clip = [ImageClip(i).set_duration(2) for i in ["老鼠會.png", "逢甲大學.jpg"]]
+video = concatenate_videoclips(clip, method = "compose")
+video.write_videofile("video.mp4", fps = 24, audio = "voice.mp3")
