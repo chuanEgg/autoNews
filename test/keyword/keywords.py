fcdf7364207b18d39289a42752fea28cc390339c
chuanEgg <honganchen53@gmail.com>
2024-01-10 11:11:03 +0800

    pack more things into functions

--- test/keyword/keywords.py
+++ test/keyword/keywords.py
@@ -1,77 +1,89 @@
 from ckiptagger import data_utils, WS, POS, NER
 import json
-
-#check if model exists
+from ckiptagger import data_utils, WS, POS, NER
 import os.path
-if not os.path.isfile("./data.zip"):
+
+def download_model():
+  if not os.path.isfile("./data.zip"):
     print("Downloading model...")
     data_utils.download_data_gdown("./") # download
     print("Downloaded model")
-else:
+  else:
     print("Model exists")
 
-with open("data.txt", "r", encoding = "utf-8") as f:
+def read_data():
+  with open("data.txt", "r", encoding = "utf-8") as f:
     data = f.read()
+  return data
 
-ws = WS("./data")
-pos = POS("./data")
-ner = NER("./data")
+def extract_keywords(data):
+  ws = WS("./data")
+  pos = POS("./data")
+  ner = NER("./data")
 
-result = []
-i = data
-line = []
-line.append(i)
-# print(line)
-seg_res = ws(line)
-pos_res = pos(seg_res)
-ner_res = ner(seg_res, pos_res)
-# print(ner_res)
-keywords = []
-skip = ['CARDINAL', 'DATE', 'TIME', 'MONEY', 'ORDINAL', 'QUANTITY', 'PERCENT']
-if len(ner_res) != 0:
-  for i in ner_res[0]:
-    # print(i[3])
-    if i[2] in skip: continue
-    keywords.append({'content': i[3],'type': i[2], 'head': i[0]})
+  result = []
+  line = []
+  line.append(data)
+  seg_res = ws(line)
+  pos_res = pos(seg_res)
+  ner_res = ner(seg_res, pos_res)
 
-idx = 0
-for i in range(len(seg_res[0])):
-  if pos_res[0][i] == 'Na':
-    if i != 0 and pos_res[0][i-1] == 'Na': # merging noun as they are likely to be refering a single object
-      keywords[-1]['content'] += seg_res[0][i]
-    elif i >= 2 and pos_res[0][i-1] == 'DE' and pos_res[0][i-2] == 'VH': # to include adj+noun (ex. 無聊的會議)
-      keywords.append({'content': seg_res[0][i-2]+seg_res[0][i], 'type': 'VH+Na', \
-                        'head': (idx-len(seg_res[0][i-1])-len(seg_res[0][i-2]))})
-    else:
-      keywords.append({'content': seg_res[0][i], 'type': 'Na', 'head': idx})
-  idx += len(seg_res[0][i])
+  keywords = []
+  skip = ['CARDINAL', 'DATE', 'TIME', 'MONEY', 'ORDINAL', 'QUANTITY', 'PERCENT']
+  if len(ner_res) != 0:
+    for i in ner_res[0]:
+      if i[2] in skip: continue
+      keywords.append({'content': i[3],'type': i[2], 'head': i[0]})
 
-idx = 0
-if len(keywords) == 0:
+  idx = 0
   for i in range(len(seg_res[0])):
-    if 'V' in pos_res[0][i] and pos_res[0][i] != 'VH':
-      if i >= 2 and pos_res[0][i-1] == 'DE' and pos_res[0][i-2] == 'VH':
-        keywords.append({'content': seg_res[0][i-2]+seg_res[0][i], 'type': 'VH+V', \
-                        'head': (idx-len(seg_res[0][i-1])-len(seg_res[0][i-2]))})
+    if pos_res[0][i] == 'Na':
+      if i != 0 and pos_res[0][i-1] == 'Na':
+        keywords[-1]['content'] += seg_res[0][i]
+      elif i >= 2 and pos_res[0][i-1] == 'DE' and pos_res[0][i-2] == 'VH':
+        keywords.append({'content': seg_res[0][i-2]+seg_res[0][i], 'type': 'VH+Na', \
+                'head': (idx-len(seg_res[0][i-1])-len(seg_res[0][i-2]))})
       else:
-        keywords.append({'content': seg_res[0][i], 'type': 'V', 'head': idx})
+        keywords.append({'content': seg_res[0][i], 'type': 'Na', 'head': idx})
     idx += len(seg_res[0][i])
 
-# print(keywords)
-# result.append(keywords)
+  idx = 0
+  if len(keywords) == 0:
+    for i in range(len(seg_res[0])):
+      if 'V' in pos_res[0][i] and pos_res[0][i] != 'VH':
+        if i >= 2 and pos_res[0][i-1] == 'DE' and pos_res[0][i-2] == 'VH':
+          keywords.append({'content': seg_res[0][i-2]+seg_res[0][i], 'type': 'VH+V', \
+                  'head': (idx-len(seg_res[0][i-1])-len(seg_res[0][i-2]))})
+        else:
+          keywords.append({'content': seg_res[0][i], 'type': 'V', 'head': idx})
+      idx += len(seg_res[0][i])
 
-to_delete = []
-for i in keywords:
-  for j in keywords:
-    if i["head"] < j["head"] and i["head"] + len(i["content"]) >= (j["head"]+len(j["content"])):
-      print(i, j)
-      to_delete.append(j)
+  return keywords
 
-keywords.remove(j)
+def remove_overlapping_keywords(keywords):
+  to_delete = []
+  for i in keywords:
+    for j in keywords:
+      if i["head"] < j["head"] and i["head"] + len(i["content"]) >= (j["head"]+len(j["content"])):
+        to_delete.append(j)
 
-sorted_keywords = sorted(keywords, key=lambda x: int(x["head"]))
-print(sorted_keywords)
+  for j in to_delete:
+    keywords.remove(j)
 
-print("Saving keywords to keywords.json")
-with open("keywords.json" ,"w", encoding = "utf-8") as f:
-    json.dump(sorted_keywords, f)
+  sorted_keywords = sorted(keywords, key=lambda x: int(x["head"]))
+  return sorted_keywords
+
+def save_keywords(keywords):
+  print("Saving keywords to keywords.json")
+  with open("keywords.json" ,"w", encoding = "utf-8") as f:
+    json.dump(keywords, f, ensure_ascii=False)
+
+def main():
+  download_model()
+  data = read_data()
+  keywords = extract_keywords(data)
+  sorted_keywords = remove_overlapping_keywords(keywords)
+  save_keywords(sorted_keywords)
+
+if __name__ == "__main__":
+  main()
--- test/news.py
+++ test/news.py
@@ -1,11 +1,27 @@
 from gnews import GNews
+import json
 
-google_news = GNews(language="zh", country="TW", max_results=5)
-res = google_news.get_news('總統大選')
-for i in range(len(res)):
-    try:
-        article = google_news.get_full_article(res[i]["url"])
-        print(article.title)
-        print(article.text)
-        print("######################################################")
-    except: pass
+def get_news(keyword):
+    google_news = GNews(language="zh", country="TW", max_results=5)
+    res = google_news.get_news(keyword)
+    result = []
+    for i in range(len(res)):
+        try:
+            article = google_news.get_full_article(res[i]["url"])
+            result.append([article.title, article.text])
+        except:
+            pass
+    return result
+
+def save_to_json(result):
+    with open("news.json", "w", encoding="utf-8") as f:
+        json.dump(result, f)
+
+def main():
+    keyword = input("請輸入關鍵字：")
+    news_result = get_news(keyword)
+    save_to_json(news_result)
+
+if __name__ == "__main__":
+  main()
+
