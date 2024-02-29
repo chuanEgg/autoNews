f78f894c6f74582e9a40abe36a93271ef8d65de5
unknown <muen1019@gmail.com>
2024-02-29 09:34:20 +0800

    edit main.py in video_v3 and test it

--- video_v2/image_and_gif.py
+++ video_v2/image_and_gif.py
@@ -10,25 +10,25 @@ apikey = "AIzaSyCmeWu09YmT9sRwyfqnZBoGROEBhsHVsF0"  # click to set to your apike
 ckey = "ytp_project"  # set the client_key for the integration and use the same value for all API calls
 
 # compare function for keywords
-def cmp(a, b):
+def keyword_cmp(a, b):
     if a["head"] < b["head"]: return -1
     if a["head"] > b["head"]: return 1
     return 0
 
 # download images from bing
 def get_image(keyword, id):
-    while True:
-        try:
-            downloader.download(keyword, \
-            limit = 1, output_dir = "image_and_video", force_replace = True, filter = "photo")
-            with open(os.path.join("image_and_video", keyword, os.listdir(os.path.join("image_and_video", keyword))[0]), "rb") as f:
-                img = f.read()
-            with open(os.path.join("image_and_video", f"{id}.jpg"), "wb") as f:
-                f.write(img)
-            rmtree(os.path.join("image_and_video", keyword))
+    # while True:
+    #     try:
+    downloader.download(keyword, \
+    limit = 1, output_dir = "image_and_video", force_replace = True, filter = "photo")
+    with open(os.path.join("image_and_video", keyword, os.listdir(os.path.join("image_and_video", keyword))[0]), "rb") as f:
+        img = f.read()
+    with open(os.path.join("image_and_video", f"{id}.jpg"), "wb") as f:
+        f.write(img)
+    rmtree(os.path.join("image_and_video", keyword))
             
-            break
-        except: pass
+            # break
+        # except: pass
 
 # download videos of GIF from tenor
 def get_gif(keyword, id):
@@ -41,20 +41,21 @@ def get_gif(keyword, id):
         vidoe_url = top_gifs["results"][0]["media_formats"]["mp4"]["url"]
         # download video
         r = requests.get(vidoe_url, stream=True)
-        with open(os.path.join("image_and_video", f"{id}.mp4"), "wb") as f:
+        with open(os.path.join("data", "image_and_video", f"{id}.mp4"), "wb") as f:
             f.write(r.content)
 
     
         
 def main():
     # sort keywords
-    with open("keyword.json", "r", encoding = "utf-8") as f:
+    # sort keywords
+    with open(os.path.join("data", "keywords.json"), "r", encoding = "utf-8") as f:
         keyword = load(f)
-    keyword = sorted(keyword, key = cmp_to_key(cmp))
+    keyword = sorted(keyword, key = cmp_to_key(keyword_cmp))
     
     # clear existed files
-    rmtree("image_and_video")
-    os.mkdir("image_and_video")
+    rmtree(os.path.join("data", "image_and_video"), ignore_errors=True)
+    os.mkdir(os.path.join("data", "image_and_video"))
     
     # get images and GIFs
     for i in range(len(keyword)):
@@ -63,6 +64,8 @@ def main():
             get_image(keyword[i]["content"], i)
         else:
             get_gif(keyword[i]["content"], i)
+    
+    print("Success for downloading image and gif!")
             
 
 if __name__ == "__main__":
